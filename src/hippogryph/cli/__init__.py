dcd544e1d6cbb3900d37c94d2b7f046cc5e428a3
Jason W. DeGraw <jason.degraw@gmail.com>
2024-04-10 02:25:57 -0400

    Fix up CLI, add workflow

--- src/hippogryph/cli/__init__.py
+++ src/hippogryph/cli/__init__.py
@@ -36,7 +36,9 @@ def guess_output_format(filename: str) -> Format|None:
 @click.option('-f', '--format', type=click.Choice(['exo', 'plot3d', 'plot3d_2d']), default=None, help='Specify format to use.')
 @click.option('-a', '--ascii', is_flag=True, show_default=True, default=False, help='Write ASCII format (if possible).')
 def channel(x_length, y_length, output, format, ascii):
-    click.echo('Generate channel grid')
+    '''
+    Generate a channel grid.
+    '''
     block = hpg.Block('domain')
     box = hpg.Box(ni=32, nj=32, block=block, left_label='west', right_label='east',
                   up_label='north', down_label='south')
@@ -52,6 +54,31 @@ def channel(x_length, y_length, output, format, ascii):
 
     mesh.apply(xgrid=xgrid, ygrid=ygrid)
 
+def validate_even_int(ctx: click.core.Context,
+                  param: click.core.Argument, value: str) -> int:
+    try:
+        v = int(value)
+        if v <= 0:
+            raise click.BadParameter('Number of elements must be positive.')
+        if v % 2 != 0:
+            raise click.BadParameter('Number of elements must be even.')
+        return v
+    except ValueError:
+        raise click.BadParameter('Number of elements must be an integer.')
+
+@click.command()
+@click.option('-n', '--number', callback=validate_even_int, show_default=True, default=32, help='Number of elements across the channel (must be even).')
+#@click.option('-z', '--z-length', type=click.File('w'), show_default=True, default=1.0, help='Length of the grid in the z direction.')
+@click.option('-o', '--output', type=click.Path(writable=True, dir_okay=False), show_default=True, default='hpg.exo', help='Write output to the specified file.')
+@click.option('-f', '--format', type=click.Choice(['exo', 'plot3d', 'plot3d_2d']), default=None, help='Specify format to use.')
+@click.option('-a', '--ascii', is_flag=True, show_default=True, default=False, help='Write ASCII format (if possible).')
+def bfs(number, output, format, ascii):
+    '''
+    Generate a backward-facing step grid
+    '''
+    mesh = hpg.backward_step(int(number/2.0))
+    mesh.write_exodusii(output)
+
 
 @click.group(context_settings={'help_option_names': ['-h', '--help']}, invoke_without_command=False)
 @click.version_option(version=__version__, prog_name='hippogryph')
@@ -60,3 +87,4 @@ def hippogryph(ctx: click.Context):
     pass
 
 hippogryph.add_command(channel)
+hippogryph.add_command(bfs)
